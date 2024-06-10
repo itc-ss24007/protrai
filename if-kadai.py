36f8626f9607c7f285640c3a839c8689c3e34da8
itc-ss24007 <s24007@std.it-college.ac.jp>
2024-06-10 15:26:09 +0900

    if-kadai.py add

---
+++ if-kadai.py
@@ -0,0 +1,12 @@
+# s24007
+# 条件文の練習
+
+score=30
+if score >=60 and score <70:
+    print("可")
+elif score >=70 and score <80:
+    print("良")
+elif score >=80:
+    print("優")
+else:
+    print("不可")
