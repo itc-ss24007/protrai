8ee5c0ca34c048d5bcccef7b92a83b5254bb4e37
itc-ss24007 <s24007@std.it-college.ac.jp>
2024-06-17 16:37:39 +0900

    time_cal.py add

---
+++ tax.py
@@ -0,0 +1,6 @@
+# tax.pyというモジュールです
+# 消費税込みの金額を返すpostTaxPrice関数のあるモジュール
+
+def postTaxPrice(price):
+    ans = price * 1.1
+    return ans
---
+++ time_cal.py
@@ -0,0 +1,10 @@
+# s24007
+# 現在の時刻と2024年6月のカレンダーを表示する
+
+import calendar
+import datetime
+
+print(calendar.month(2024,6))
+
+now=datetime.datetime.now()
+print(now) 
