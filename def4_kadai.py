701acc6ad1c2b16712b12e34f363c4e1d06d2f4c
itc-ss24007 <s24007@std.it-college.ac.jp>
2024-06-17 15:52:12 +0900

    modified def4_kadai.py

--- def4_kadai.py
+++ def4_kadai.py
@@ -1,12 +1,18 @@
 # s24007
-#
+# メインの処理をmain()関数に行わせるおみくじプログラム
 
 import random
 
-def main():
+# ランダムでkujiの中の一つを返す関数
+def omikuji():
     kuji=['大吉','中吉','小吉','凶']
     return random.choice(kuji)
 
+# エントリーポイントの定義
+def main():
+    kekka = omikuji()
+    print("結果は",kekka,"です。")
+
 if __name__ == "__main__":
-    print("結果は",main(),"です。")
+    main()
     
